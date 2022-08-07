5a139d04373a27e9a5cc68a1faf8cb843eb53f9c
Omelchenko-Andrey <paarkourz@gmail.com>
2022-08-07 23:13:04 +0300

    contacts + locations + btn-up

---
+++ src/js/modal-locations.js
@@ -0,0 +1,16 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-locations-open]"),
+    closeModalBtn: document.querySelector("[data-modal-locations-close]"),
+    modal: document.querySelector("[data-modal-locations]"),
+    body: document.querySelector("[data-body-locations]"),
+  };
+
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+    refs.body.classList.toggle("no-scroll");
+  }
+})();
