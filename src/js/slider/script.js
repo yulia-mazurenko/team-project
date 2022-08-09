a042b2fce9821bff1ecdab056a2cfd88b5997fff
ruchey34 <me.oleg1@gmail.com>
2022-08-09 12:31:49 +0300

    update slider

---
+++ src/js/slider/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider').slick({
+    dots: true,
+    infinite: false,
+    adaptiveHeight: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1
+});
+});
