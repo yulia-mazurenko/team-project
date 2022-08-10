e0e2d9d31b76e33ddf84fea15e1bd37ebe8360d8
yulia-mazurenko <106391487+yulia-mazurenko@users.noreply.github.com>
2022-08-10 20:14:20 +0000

    Deploying to gh-pages from @ yulia-mazurenko/team-project@e217ad3460b864092ed0bd5bd00d395e17d264b0 🚀

---
+++ index.b1d54dd8.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-about-open]"),closeModalBtn:document.querySelector("[data-modal-about-close]"),modal:document.querySelector("[data-modal-about]"),body:document.querySelector("[data-body-about]")};function e(){o.modal.classList.toggle("is-hidden"),o.body.classList.toggle("no-scroll")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.b1d54dd8.js.map
